644539a114d6583ea244cdb55c928532fb7277e7
pank0ff <90576203+pank0ff@users.noreply.github.com>
2021-11-07 15:56:25 +0300

    CREATE class AccountingSystem with functions for search, add, search information, delete and compare home and apartments

---
+++ src/main/java/AccountingSystem.java
@@ -0,0 +1,144 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+public class AccountingSystem {
+    private final ArrayList<House> houses = new ArrayList<House>();
+
+    public House findHouseByNumber(int number) {
+        for (House i : houses) {
+            if (i.getNumber() == number) {
+                return i;
+            }
+        }
+        House temp = new House("empty");
+        temp.setFlatCount(-1);
+        return temp;
+    }
+
+    public Flat findFlatByNumber(@NotNull House house, int number) {
+        for (Flat i : house.getFlat()) {
+            if (i.getNumber() == number) {
+                return i;
+            }
+        }
+        Flat temp = new Flat("empty");
+        temp.setNumber(-1);
+        return temp;
+    }
+
+    public void addHouse(int number) {
+        for (House i : houses) {
+            if (i.getNumber() == number) {
+                System.out.println("A house with this number exists");
+                return;
+            }
+        }
+        House house = new House(number);
+        houses.add(house);
+        System.out.println("House added successfully");
+    }
+
+    public void addFlat(int numberOfHome) {
+        House temp = findHouseByNumber(numberOfHome);
+        if (temp.getFlatCount() == -1) {
+            System.out.println("There is no house with this number.");
+            return;
+        } else {
+            Flat tempFlat = new Flat();
+            temp.addFlat(tempFlat);
+            System.out.println("Flat added successfully");
+        }
+    }
+
+    public void getFlatInformation(int numberOfHome, int numberOfApartment) {
+        House temp = findHouseByNumber(numberOfHome);
+        if (temp.getFlatCount() == -1) {
+            System.out.println("There is no such house");
+            return;
+        }
+        Flat temp2 = temp.getFlat(numberOfApartment);
+        if (temp.getNumber() == -1) {
+            System.out.println("Such an flat does not exist");
+            return;
+        }
+        temp2.printInfo();
+    }
+
+    public void getHouseInformation(int number) {
+        House temp = findHouseByNumber(number);
+        if (temp.getFlatCount() == -1) {
+            System.out.println("There is no such house");
+            return;
+        }
+        for (Flat i : temp.getFlat()) {
+            i.printInfo();
+        }
+        if (temp.getFlat().size() == 0) {
+            System.out.println("There are no flats in the house");
+        }
+    }
+
+    public void removeHouse(int number) {
+        House temp = findHouseByNumber(number);
+        if (temp.getFlatCount() == -1) {
+            System.out.println("There is no such house");
+            return;
+        }
+        houses.remove(temp);
+    }
+
+    public void removeFlat(int numberOfHome, int numberOfApartment) {
+        House temp = findHouseByNumber(numberOfHome);
+        if (temp.getFlatCount() == -1) {
+            System.out.println("There is no such house");
+            return;
+        }
+        Flat temp1 = findFlatByNumber(temp, numberOfApartment);
+        if (temp1.getNumber() == -1) {
+                System.out.println("There is no such flat");
+            return;
+        }
+        temp.removeFlat(temp1);
+    }
+
+
+    public void compareFlat(int numberOfHouse1, int numberOfHouse2, int numberOfFlat1, int numberOfFlat2) {
+
+        House house1 = findHouseByNumber(numberOfHouse1);
+        if (house1.getFlatCount() == -1) {
+            System.out.println("There is no such house");
+            return;
+        }
+        Flat flat1 = findFlatByNumber(house1, numberOfFlat1);
+        if (flat1.getNumber() == -1) {
+            System.out.println("There is no such flat");
+            return;
+        }
+        House house2 = findHouseByNumber(numberOfHouse2);
+        if (house2.getFlatCount() == -1) {
+            System.out.println("There is no such house");
+            return;
+        }
+        Flat flat2 = findFlatByNumber(house2, numberOfFlat2);
+        if (flat2.getNumber() == -1) {
+            System.out.println("There is no such flat");
+            return;
+        }
+        flat1.compareBy(flat2);
+    }
+
+    public void compareHouse(int numberOfHouseOne, int numberOfHouseTwo) {
+        House houseOne = findHouseByNumber(numberOfHouseOne);
+        if (houseOne.getFlatCount() == -1) {
+            System.out.println("This house does not exist");
+            return;
+        }
+        House houseTwo = findHouseByNumber(numberOfHouseTwo);
+        if (houseTwo.getFlatCount() == -1) {
+            System.out.println("This house does not exist");
+            return;
+        }
+        houseOne.compareBy(houseTwo);
+    }
+}
